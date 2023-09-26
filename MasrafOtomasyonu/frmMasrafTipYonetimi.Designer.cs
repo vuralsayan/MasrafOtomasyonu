5c1dee976f960ecfa7e51543e4abaa7d89ab2605
Vural Mert Sayan <vuralsayan01@gmail.com>
2023-09-26 11:25:14 +0300

    Masraf tipleri form tasarımı tamamlandı

---
+++ MasrafOtomasyonu/frmMasrafTipYonetimi.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace MasrafOtomasyonu
+{
+    partial class frmMasrafTipYonetimi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMasrafTipi = new System.Windows.Forms.TextBox();
+            this.btnYeniEkle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lstMasrafTipleri = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtMasrafTipi);
+            this.groupBox1.Controls.Add(this.btnYeniEkle);
+            this.groupBox1.Controls.Add(this.btnKaydet);
+            this.groupBox1.Controls.Add(this.btnSil);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(10, 341);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(611, 99);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ekleme/Düzenleme/Silme";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Masraf / Harcama Tipi";
+            // 
+            // txtMasrafTipi
+            // 
+            this.txtMasrafTipi.Location = new System.Drawing.Point(10, 66);
+            this.txtMasrafTipi.Name = "txtMasrafTipi";
+            this.txtMasrafTipi.Size = new System.Drawing.Size(296, 26);
+            this.txtMasrafTipi.TabIndex = 9;
+            // 
+            // btnYeniEkle
+            // 
+            this.btnYeniEkle.Location = new System.Drawing.Point(326, 51);
+            this.btnYeniEkle.Name = "btnYeniEkle";
+            this.btnYeniEkle.Size = new System.Drawing.Size(88, 41);
+            this.btnYeniEkle.TabIndex = 5;
+            this.btnYeniEkle.Text = "Yeni Ekle";
+            this.btnYeniEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(420, 51);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(88, 41);
+            this.btnKaydet.TabIndex = 6;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(514, 51);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(88, 41);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(10, 331);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(611, 10);
+            this.panel1.TabIndex = 1;
+            // 
+            // lstMasrafTipleri
+            // 
+            this.lstMasrafTipleri.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstMasrafTipleri.FormattingEnabled = true;
+            this.lstMasrafTipleri.ItemHeight = 20;
+            this.lstMasrafTipleri.Location = new System.Drawing.Point(10, 10);
+            this.lstMasrafTipleri.Name = "lstMasrafTipleri";
+            this.lstMasrafTipleri.Size = new System.Drawing.Size(611, 321);
+            this.lstMasrafTipleri.TabIndex = 2;
+            // 
+            // frmMasrafTipYonetimi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 450);
+            this.Controls.Add(this.lstMasrafTipleri);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMasrafTipYonetimi";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Masraf Tipleri";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox lstMasrafTipleri;
+        private System.Windows.Forms.Button btnYeniEkle;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMasrafTipi;
+    }
+}
